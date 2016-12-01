5f17a0b779c19a0fb6c6558c7ac4546f60fb76db
townmi <towne766@126.com>
2016-12-02 00:07:48 +0800

    question: how to set struct in map data?

--- rest/rest.go
+++ rest/rest.go
@@ -2,9 +2,10 @@ package rest
 
 import (
 	"net/http"
+	"fmt"
 )
 
-type route struct {
+type routeInfo struct {
 	url string
 	method string
 	handler HandlerFunc
@@ -14,25 +15,29 @@ type HandlerFunc func(http.ResponseWriter, *http.Request)
 
 
 type Init struct {
-	route map[int]struct{}
+	Route map[string]HandlerFunc
 }
+
+var R = &Init{make(map[string]HandlerFunc)}
 
 func (R *Init) Map() {
 
 }
 
 func (R *Init) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(len(R.Route))
 	if r.URL.Path == "/" {
+		fmt.Println("123")
 		return
 	}
 	http.NotFound(w, r)
 	return
 }
 
-
 /**
     register router
  */
-func (R *Init) Post(r, h HandlerFunc) {
-	R.route[len(R.route)] = route{url: "/", method:"POST", "handler": h}
+func (R *Init) Post(url string, h HandlerFunc) {
+	rInfo := routeInfo{url, "POST", h}
+	R.Route[url] = rInfo
 }
