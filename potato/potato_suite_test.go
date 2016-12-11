d58af07e48f9be61a3be5a2821a71a7b900fa6f8
townmi <towne766@126.com>
2016-12-11 21:52:11 +0800

    set agouti with run script

--- potato/potato_suite_test.go
+++ potato/potato_suite_test.go
@@ -2,23 +2,51 @@ package potato_test
 
 import (
 	"testing"
-	. "github.com/onsi/ginkgo"
+	_ "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/sclevine/agouti"
+	"fmt"
+	"bytes"
 )
 
-func TestPotato(t *testing.T) {
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Potato Suite")
+//var (
+//	Expect := gomega.Expect
+//	Succeed := gomega.Succeed
+//	HaveOccurred := gomega.HaveOccurred
+//	HaveText := matchers.HaveText
+//	HaveURL := matchers.HaveURL
+//)
+
+
+
+func TestUserLoginPrompt(t *testing.T) {
+
+	RegisterTestingT(t)
+	b := bytes.NewBuffer(make([]byte, 0))
+	driver := agouti.Selenium()
+	Expect(driver.Start()).To(Succeed())
+	page, err := driver.NewPage(agouti.Browser("chrome"))
+	Expect(err).NotTo(HaveOccurred())
+
+	page.Size(200, 300)
+
+	page.Navigate("http://lp.vipabc.com/program/linkage_page/newjsapitest/index.html")
+	var number int
+
+	page.RunScript("alert(1);", map[string]interface{}{"test": 100}, &number)
+
+	//page.Find("#prompt")).To(HaveText("Please login!"))
+
+	//t:= reflect.TypeOf(page);
+
+	//fmt.Println(page.HTML())
+
+
+
+
+
+	//fmt.Println("type:", reflect.TypeOf(ab))
+
+	fmt.Fprintln(b)
+	// Expect(driver.Stop()).To(Succeed()) // calls page.Destroy() automatically
 }
-
-var agoutiDriver *agouti.WebDriver
-
-var _ = BeforeSuite(func() {
-	agoutiDriver = agouti.Selenium()
-	Expect(agoutiDriver.Start()).To(Succeed())
-})
-
-var _ = AfterSuite(func() {
-	Expect(agoutiDriver.Stop()).To(Succeed())
-})
