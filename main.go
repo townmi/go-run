c0271f3ed875fdc4d4890c656cde7b2eb4b25233
townmi <towne766@126.com>
2016-12-18 23:22:03 +0800

    set agouti with run script

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"go-run/rest"
-	"go-run/route"
-)
+ import (
+ 	"log"
+ 	"net/http"
+ 	"go-run/rest"
+ 	"go-run/route"
+ )
 
 
-func main(){
-	var app = *rest.R
+ func main(){
+ 	var app = *rest.R
 
-	err := http.ListenAndServe(":9090", rest.R) //设置监听的端口
+ 	err := http.ListenAndServe(":9090", rest.R) //设置监听的端口
 
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+ 	if err != nil {
+ 		log.Fatal("ListenAndServe: ", err)
+ 	}
 
-	app.Get("/", route.GetHome);
+ 	app.Get("/", route.GetHome);
 
-	app.Get("/2", route.GetHome);
+ 	app.Get("/2", route.GetHome);
 
-	app.Post("/", route.PostHome);
+ 	app.Post("/", route.PostHome);
 
-}
+ }
--- rest/rest.go
+++ rest/rest.go
@@ -26,7 +26,7 @@ func (R *Init) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	key := strings.ToUpper(r.Method) + strings.ToUpper(r.URL.Path)
 
 	if url := R.Route[key].url; len(url) != 0 {
-		R.Route[key].handler(w, r);
+		R.Route[key].handler(w, r)
 		return
 	}
 	http.NotFound(w, r)
@@ -34,7 +34,11 @@ func (R *Init) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 /**
-    register router
+ * [Post register router]
+ * @Author   townmi
+ * @DateTime 2016-12-13T22:42:56+0800
+ * @url    {[string]} 	[description]
+ * @h      {[HandlerFunc]}	[description]
  */
 func (R *Init) Post(url string, h HandlerFunc) {
 
@@ -69,3 +73,33 @@ func (R *Init) Delete(url string, h HandlerFunc) {
 	rInfo := routeInfo{url, "DELETE", h}
 	R.Route[key] = rInfo
 }
+
+/**
+ * use method
+ */
+
+// import (
+// 	"log"
+// 	"net/http"
+// 	"go-run/rest"
+// 	"go-run/route"
+// )
+
+
+// func main(){
+// 	var app = *rest.R
+
+// 	err := http.ListenAndServe(":9090", rest.R) //设置监听的端口
+
+// 	if err != nil {
+// 		log.Fatal("ListenAndServe: ", err)
+// 	}
+
+// 	app.Get("/", route.GetHome);
+
+// 	app.Get("/2", route.GetHome);
+
+// 	app.Post("/", route.PostHome);
+
+// }
+
