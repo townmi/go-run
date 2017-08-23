864dd18297166be8f076308e19636ddedee2e9ef
townmi <towne766@126.com>
2017-08-23 13:38:27 +0800

    init

--- main.go
+++ main.go
@@ -2,43 +2,24 @@ package main
 
 import (
 	"net/http"
-	"go-run/config"
-	_ "log"
-	"go-run/route"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/handlers"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	// new mux Router
-	r := mux.NewRouter()
-	//
-	r.Host("www.example.com")
-	//r.Handle("/statics/{rest}", http.StripPrefix("/statics/", http.FileServer(http.Dir(config.Env.PATH + "statics/"))))
-	r.PathPrefix("/statics/").Handler(http.StripPrefix("/statics/", http.FileServer(http.Dir(config.Env.PATH + "statics/"))))
+	// Echo instance
+	e := echo.New()
 
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
-	// GET ROUTES MAP
-	r.HandleFunc("/", route.GetHome).Methods("GET")
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
 
-	r.HandleFunc("/search", route.GetSearch).Methods("GET")
-
-	r.HandleFunc("/email", route.SendEmail).Methods("GET")
-
-	r.HandleFunc("/stock", route.GetStock).Methods("GET")
-	r.HandleFunc("/stocklist", route.GetStockList).Methods("GET")
-
-	r.HandleFunc("/stocklistcheck", route.CheckStockList).Methods("GET")
-
-	r.HandleFunc("/house", route.GetHouse).Methods("GET")
-
-	// POST ROUTES MAP
-	r.HandleFunc("/search", route.PostSearch).Methods("POST")
-
-	r.HandleFunc("/stocklist", route.ReFreshStockList).Methods("POST")
-	r.HandleFunc("/stock", route.ReFreshStock).Methods("POST")
-
-	// BIND PORT TO SERVER
-	http.ListenAndServe(":" + config.Env.PORT, handlers.CompressHandler(r))
-
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
 }
